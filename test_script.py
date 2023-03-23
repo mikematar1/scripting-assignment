357fc20cb4ecb8b27e5af2aa9b3580eb670b04c3
Michael Matar <69992862+mikematar1@users.noreply.github.com>
2023-03-23 03:34:28 +0200

    fixing bugs

--- test_script.py
+++ test_script.py
@@ -1,28 +1,37 @@
 import requests,sys,re
+
+import urllib
 website = sys.argv[1]
+subfile=open("subdoms","w")
+dirfile = open("dirs",'w')
+with open("subdomains_dictionary.bat",'r') as subdomains:
+    for line in subdomains:
+        line = re.sub("\s+","",line)
+        sub = f"https://{line}.{website}"
+        try:
+                response = urllib.request.urlopen(sub)
+        except urllib.error.URLError:
+                continue
+        if response.code == 200:
+            subfile.write(sub+"\n")
+with open("dirs_dictionary.bat",'r') as dictionaries:
+      for line in dictionaries:
+            line = re.sub("\s+","",line)
+            directory = f"https://{website}/{line}"
+            try:
+                response = urllib.request.urlopen(directory)
+            except urllib.error.URLError:
+                    continue
+            if response.code == 200:
+                dirfile.write(directory+"\n")
 response = requests.get(website)
 html = response.text
-print(html)
-pattern = 'href="[^\s]*"'
-func = lambda x : x[6:-1]
-links =re.findall(pattern,html)
-print(links)
-# for x in links:
-#     print(x)
-links = map(func,links)
-links = list(set(links))
-# print(links)
-# for x in links:
-#     print(x)
-directory=open("dirs","w") 
-sub=open("subdoms","w")
 files=open("files","w")
+pattern = 'href="[^\s]*"'
+links =re.findall(pattern,html)
 for link in links:
-    l = link.split("/")
-    print(link)
-    if re.search("^https?:\/\/(?:[^\/]+\.)*com(\/)?$",link):
-        sub.write(link + "\n")
-    elif re.match(".*\.[^c]*[^o]*[^m]*",l[-1]):
+    l = link.split("/") 
+    if re.match(".*\.[^c]*[^o]*[^m]*",l[-1]):
         files.write(link + "\n")
-    else:
-        directory.write(link+"\n")
+   
+
